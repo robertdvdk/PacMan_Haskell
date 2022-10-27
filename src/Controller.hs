30b31a9dc63f2890b6725f804f1b82f7771bebcb
jurre1010 <jurre1010@hotmail.com>
2022-10-27 19:01:46 +0200

    changes

--- src/Controller.hs
+++ src/Controller.hs
@@ -13,8 +13,9 @@ import Model (Player)
 step :: Float -> GameState -> IO GameState
 step secs gstate
   | elapsedTime gstate + secs > nO_SECS_BETWEEN_CYCLES
-  = -- We show a new random number
-    do return $ movePlayer (GameState (ShowAPlayer (getPlayer gstate)) 0 Playing (getPlayer gstate))
+    do return $ updatePlayer --  Make a bigger update function that updates the player (if pac man can change direction, change direction)
+                              -- and doe movePlayer or later or make it part of updatePlayer    
+    movePlayer (GameState (ShowAPlayer (player gstate)) 0 Playing (player gstate))
   | otherwise
   = -- Just update the elapsed time
     return $ gstate { elapsedTime = elapsedTime gstate + secs }
@@ -25,33 +26,38 @@ input e gstate = return (inputKey e gstate)
 
 inputKey :: Event -> GameState -> GameState
 inputKey (EventKey (Char c) _ _ _) gstate
-  -- If the user presses a character key, show that one
-    | c == 'w'    = gstate { player = playerChangeDirection North (getPlayer gstate)}
-    | c == 's'    = gstate { player = playerChangeDirection South (getPlayer gstate)}
-    | c == 'a'    = gstate { player = playerChangeDirection West (getPlayer gstate)}
-    | c == 'd'    = gstate { player = playerChangeDirection East (getPlayer gstate)}
+    | c == 'w'    = gstate { player = playerChangeNextDirection North (player gstate)}
+    | c == 's'    = gstate { player = playerChangeNextDirection South (player gstate)}
+    | c == 'a'    = gstate { player = playerChangeNextDirection West (player gstate)}
+    | c == 'd'    = gstate { player = playerChangeNextDirection East (player gstate)}
     | otherwise   = gstate { infoToShow = ShowAChar c }
-inputKey _ gstate = gstate -- Otherwise keep the same
+inputKey _ gstate = gstate 
+
+playerChangeNextDirection :: Direction -> Player -> Player  -- When wasd is pressed, first the playerNextDirection changes
+playerChangeNextDirection dir player = player { playerNextDirection = dir }
+
+ableToChangeDirection :: Level -> Player -> Bool  -- Checks if pac-man can make a turn (now set to true because there is no check yet)
+ableToChangeDirection level player = True
+
+playerChangeDirection :: Level -> Player -> Player -- Needs to be checked the whole time, how?
+playerChangeDirection level player  | ableToChangeDirection level player = player { playerDirection = playerNextDirection player}  
+                                    | Otherwise = Nothing       -- If pac-man can make a move fill nextdirection in direction
+
+movePlayer :: GameState -> GameState  
+movePlayer gstate = gstate { elapsedTime = 0, player = move (player gstate) }
+
+move :: Player -> Player          -- Change location based on the direction 
+move player = case playerDirection player of 
+  West -> player { playerLocation = (x - 5, y) }
+  East -> player { playerLocation = (x + 5, y) }
+  North -> player { playerLocation = (x, y + 5) }
+  South -> player { playerLocation = (x, y - 5) }
+  where (x, y) = playerLocation player
 
 pauseGame :: GameState -> IO GameState
 pauseGame gstate = do c <- getChar
                       case c of
-                            ' ' -> changePauseState gstate where
-                              changePauseState (GameState a b Playing c) = return $ GameState a b Paused c
-                              changePauseState (GameState a b Paused c) = return $ GameState a b Playing c
+                            ' ' -> case playState gstate of
+                              Playing -> return $ gstate {playState = Paused}
+                              Paused  -> return $ gstate {playState = Playing}
 
-movePlayer :: GameState -> GameState
-movePlayer (GameState a b c player) = GameState a 0 c (move player)
-
-move :: Player -> Player
-move (Player (x, y) dir nextdir lives) = case dir of 
-  West -> Player (x - 5, y) dir nextdir lives
-  East -> Player (x + 5, y) dir nextdir lives
-  North -> Player (x, y + 5) dir nextdir lives
-  South -> Player (x, y - 5) dir nextdir lives
-
-getPlayer :: GameState -> Player
-getPlayer (GameState a b c player) = player
-
-playerChangeDirection :: Direction -> Player -> Player
-playerChangeDirection dir (Player loc playerdir playernextdir lives) = Player loc dir playernextdir lives
