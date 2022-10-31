082c6c2f3f5f987545c7dab7b43af5c99ba75941
robert.vanderklis <robert.vanderklis@wur.nl>
2022-10-31 12:34:02 +0100

    movement and first level done

--- src/View.hs
+++ src/View.hs
@@ -16,12 +16,16 @@ viewPure gstate = case infoToShow gstate of
   ShowAPlayer p -> translate p1 p2 (color yellow (Circle 10))
                     where (p1, p2) = playerLocation p
   ShowGame l p -> drawLevel l p
+  -- ShowGame l p -> drawLevel l
 
 
 
 getLocation :: Player -> (Float, Float)
 getLocation (Player a _ _ _) = a
 
+-- drawLevel :: [Point] -> Player -> Picture
+-- drawLevel level player
+  -- = pictures (translate p1*20 p2*20 (color yellow (Circle 20)) : [translate (fst point * 20) (snd point * 20) (color blue (rectangleSolid 10 10)) | point <- level]) where (p1, p2) = playerLocation player
 drawLevel :: [Point] -> Player -> Picture
-drawLevel level player
-  = pictures (translate p1 p2 (color yellow (Circle 10)) : [translate (fst point) (snd point) (color blue (rectangleSolid 10 10)) | point <- level]) where (p1, p2) = playerLocation player
+-- drawLevel level player = pictures (translate p1*20 p2*20 (color yellow (Circle 20)) : [translate (fst point * 20) (snd point * 20) (color blue (rectangleSolid 20 20)) | point <- level]) where (p1, p2) = playerLocation player
+drawLevel level player = pictures (translate (p1 * 10) (p2 * 10) (color yellow (Circle 5)) : ([translate (fst point * 10) (snd point * 10) (color blue (rectangleSolid 10 10)) | point <- level])) where (p1, p2) = playerLocation player
