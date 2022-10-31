72eac62b13724068a35fa07bb5e3bafb9a983295
robert.vanderklis <robert.vanderklis@wur.nl>
2022-10-31 11:07:17 +0100

    first level

--- src/Model.hs
+++ src/Model.hs
@@ -54,6 +54,7 @@ data Color = Red | Pink | Yellow | Blue
 initialState :: GameState
 -- initialState = GameState ShowNothing 0 Begin (Player (50, 50) East West 3) []
 -- level1 = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (5, 1), (5, 2), (5, 3), (5, 4), (5, 5)]
-level1 = [(x, -100) | x <- [-100..200]] ++ [(-100, y) | y <- [-200.. -100]]
-
-initialState = GameState ShowNothing 0 Begin (Player (50, 50) East West 3) level1
+makeLevelRectangle :: Location -> Location -> Level
+makeLevelRectangle (x1, y1) (x2, y2) = [(x, y) | x <- [x1..x2], y <- [y1, y2]] ++ [(x, y) | x <- [x1, x2], y <- [y1..y2]]
+level1 = makeLevelRectangle (-180, -180) (180, 180) ++ makeLevelRectangle (-50, 130) (50, 180) ++ makeLevelRectangle (-150, 0) (-80, 150) ++ makeLevelRectangle (80, 80) (150, 150) ++ makeLevelRectangle (-50, 0) (50, 100) ++ makeLevelRectangle (-150, -60) (50, -30) ++ makeLevelRectangle (80, -60) (150, 50) ++ makeLevelRectangle (-150, -90) (150, -90) ++ makeLevelRectangle (-150, -120) (0, -120) ++ makeLevelRectangle (30, -120) (150, -120) ++ makeLevelRectangle (-150, -150) (-30, -150) ++ makeLevelRectangle (0, -150) (150, -150)
+initialState = GameState ShowNothing 0 Begin (Player (0, -20) North North 3) level1
